cbf417c5cb6717e7f9f26a377aee21a8cc8ad44b
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-10-05 10:12:28 +0700

    fixed some typos and readme update

--- src/components/signin/Google.tsx
+++ src/components/signin/Google.tsx
@@ -4,7 +4,7 @@ import styles from '@bwsite/styles/signinpage.module.css'
 export default function GoogleSignInButton() {
   return (
     <button className={styles.button}>
-      <Image src={"/google.svg"} alt="google" width={20} height={20} className='absolute' />
+      <Image src={"/Google.svg"} alt="google" width={20} height={20} className='absolute' />
       <span className='text-center w-full'>
         Google
       </span>
